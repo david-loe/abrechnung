3f6d35c0416950ec90b5f644ba3cdc755bdf5da5
david-loe <d.loewens24@gmail.com>
2023-03-04 09:09:30 +0000

    adding flag to currency

--- backend/models/currency.js
+++ backend/models/currency.js
@@ -7,7 +7,8 @@ const currencySchema = new mongoose.Schema({
   },
   _id: { type: String, required: true, trim: true, alias: 'code' },
   subunit: { type: String, trim: true },
-  symbol: { type: String, trim: true }
+  symbol: { type: String, trim: true },
+  flag: { type: String }
 })
 
 
