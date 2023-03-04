8213c3ab3e38f6325e98163e93d735c26e71da0d
david-loe <d.loewens24@gmail.com>
2023-03-04 09:06:56 +0000

    adding vue-select

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import vSelect from './vue-select';
+import 'vue-select/dist/vue-select.css';
+import "./vue-select.css"
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import "bootstrap-icons/font/bootstrap-icons.css"
 
+
+
 import i18n from './i18n'
 
-createApp(App).use(i18n).use(router).mount('#app')
+createApp(App).component('vSelect', vSelect).use(i18n).use(router).mount('#app')
