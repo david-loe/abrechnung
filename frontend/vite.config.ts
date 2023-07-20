3eb24f2a9589f87f5996509a3993ee8096f5c50c
David Loe <d.loewens24@gmail.com>
2023-07-20 16:46:34 +0200

    more ts

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default {
+  plugins: [vue()],
+  server: { port: 80, strictPort: true, host: '0.0.0.0' },
+  preview: { port: 80, host: '0.0.0.0' },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  }
+}
