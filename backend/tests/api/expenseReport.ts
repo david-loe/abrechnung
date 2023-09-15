9d97d9b3d5a9a1f3ac6de195248b76a87945afc9
David Loe <d.loewens24@gmail.com>
2023-09-15 15:07:58 +0200

    adding tests

---
+++ backend/tests/api/expenseReport.ts
@@ -0,0 +1,110 @@
+import test from 'ava'
+import { Expense, ExpenseReport, ExpenseReportSimple } from '../../../common/types.js'
+import createAgent, { loginExamine, loginUser } from './_agent.js'
+import { objectToFormFields } from './_helper.js'
+
+const agent = createAgent()
+await loginUser(agent)
+
+//@ts-ignore
+var expenseReport: ExpenseReportSimple = {
+  name: 'Expenses from last Month'
+}
+
+test.serial('POST /expenseReport/inWork', async (t) => {
+  const res = await agent.post('/api/expenseReport/inWork').send(expenseReport)
+  expenseReport = res.body.result
+  t.is(res.status, 200)
+})
+
+test.serial('GET /expenseReport', async (t) => {
+  t.plan(2)
+  const res = await agent.get('/api/expenseReport')
+  t.is(res.status, 200)
+  for (const gotExpenseReport of res.body.data as ExpenseReportSimple[]) {
+    if (expenseReport._id === gotExpenseReport._id) {
+      t.pass()
+      break
+    }
+  }
+})
+
+// FILL OUT
+
+const expenses: Expense[] = [
+  {
+    description: 'English Course',
+    cost: {
+      amount: 82, //@ts-ignore
+      currency: { _id: 'GBP' }, //@ts-ignore
+      receipts: [{ name: 'Online Invoice.pdf', type: 'application/pdf', data: 'tests/files/dummy.pdf' }],
+      date: new Date('2023-09-14T00:00:00.000Z')
+    }
+  },
+  {
+    description: 'Dinner with customer',
+    cost: {
+      amount: 700, //@ts-ignore
+      currency: { _id: 'CNY' },
+      receipts: [
+        //@ts-ignore
+        { name: 'Photo.jpg', type: 'image/jpeg', data: 'tests/files/dummy.jpg' }, //@ts-ignore
+        { name: 'Photo2.jpg', type: 'image/jpeg', data: 'tests/files/small-dummy.jpg' }
+      ],
+      date: new Date('2023-09-13T00:00:00.000Z')
+    }
+  }
+]
+
+test.serial('POST /expenseReport/expense', async (t) => {
+  t.plan(expenses.length + 0)
+  for (const expense of expenses) {
+    var req = agent.post('/api/expenseReport/expense')
+    for (const entry of objectToFormFields(expense)) {
+      if (entry.field.length > 6 && entry.field.slice(-6) == '[data]') {
+        req = req.attach(entry.field, entry.val)
+      } else {
+        req = req.field(entry.field, entry.val)
+      }
+    }
+    const res = await req.field('expenseReportId', expenseReport._id.toString())
+    t.is(res.status, 200)
+  }
+})
+
+test.serial('POST /expenseReport/underExamination', async (t) => {
+  t.plan(4)
+  const comment = "A quite long comment but this doesn't matter because mongoose has no limit."
+  const res = await agent.post('/api/expenseReport/underExamination').send({ _id: expenseReport._id, comment })
+  t.is(res.status, 200)
+  t.is((res.body.result as ExpenseReport).state, 'underExamination')
+  t.is((res.body.result as ExpenseReport).history.length, 1)
+  t.like((res.body.result as ExpenseReport).comments[0], { text: comment, toState: 'underExamination' })
+})
+
+// EXAMINE
+
+test.serial('POST /examine/expenseReport/refunded', async (t) => {
+  await loginExamine(agent)
+  t.plan(4)
+  const comment = '' // empty string should not create comment
+  const res = await agent.post('/api/examine/expenseReport/refunded').send({ _id: expenseReport._id, comment })
+  t.is(res.status, 200)
+  t.is((res.body.result as ExpenseReport).state, 'refunded')
+  t.is((res.body.result as ExpenseReport).history.length, 2)
+  t.is((res.body.result as ExpenseReport).comments.length, 1)
+})
+
+// REPORT
+
+test.serial('GET /expenseReport/report', async (t) => {
+  await loginUser(agent)
+  const res = await agent.get('/api/expenseReport/report').query({ id: expenseReport._id })
+  t.is(res.status, 200)
+})
+
+test.after.always('DELETE /expenseReport', async (t) => {
+  await loginUser(agent)
+  const res = await agent.delete('/api/expenseReport').query({ id: expenseReport._id })
+  t.is(res.status, 200)
+})
