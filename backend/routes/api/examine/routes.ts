cc4a2d19bb117665579682dc695a5917ad9a7808
David Loe <d.loewens24@gmail.com>
2023-09-15 15:07:50 +0200

    adding expenseReport examine routes

--- backend/routes/api/examine/routes.ts
+++ backend/routes/api/examine/routes.ts
@@ -2,8 +2,10 @@ import express from 'express'
 const router = express.Router()
 import travelRoutes from './travel.js'
 import documentFileRoutes from './documentFile.js'
+import expenseReportRoutes from './expenseReport.js'
 
 router.use('/travel', travelRoutes)
 router.use('/documentFile', documentFileRoutes)
+router.use('/expenseReport', expenseReportRoutes)
 
 export default router
