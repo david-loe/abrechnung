17232ed44df5b4713f8896edc3239333fc5c3ab7
d.loavid-loe <d.loewens24@gmail.com>
2023-09-19 12:50:03 +0200

    refactor PaginationList

--- frontend/src/components/travel/elements/TravelCardList.vue
+++ frontend/src/components/travel/elements/TravelCardList.vue
@@ -1,84 +1,39 @@
 <template>
-  <div style="min-height: 120px">
-    <template v-if="hasTravels">
-      <div class="row justify-content-center gx-4 gy-2">
-        <div class="col-auto" v-for="travel of travels" :key="travel._id">
-          <TravelCard
-            v-if="travel"
-            :travel="travel"
-            :showTraveler="showTraveler"
-            :showDropdown="showDropdown"
-            @clicked="$emit('clicked', travel)"
-            @deleted="deleteTravel(travel._id)"
-            @edit="$emit('edit', travel)"></TravelCard>
-        </div>
-      </div>
-      <div v-if="travels.length === 0" class="alert alert-light" role="alert">
-        {{ $t('alerts.noTravels.' + endpoint) }}
-      </div>
-      <div v-else-if="meta && meta.countPages > 1" class="mx-auto text-secondary" style="width: fit-content">
-        <span class="me-2 p-2" style="cursor: pointer" @click="getTravels(meta.page - 1)">&lt;</span>
-        <span
-          v-for="x of meta.countPages"
-          :class="'me-2 p-2' + (meta.page == x ? ' fs-5' : '')"
-          :key="x"
-          :style="meta.page == x ? '' : 'cursor: pointer;'"
-          @click="meta.page == x ? null : getTravels(x)"
-          >{{ x }}</span
-        >
-        <span class="p-2" style="cursor: pointer" @click="getTravels(meta.page + 1)">&gt;</span>
-      </div>
+  <PaginationList :endpoint="endpoint" ref="list">
+    <template #entry="{ entry }">
+      <TravelCard
+        :travel="entry"
+        :showTraveler="showTraveler"
+        :showDropdown="showDropdown"
+        @clicked="$emit('clicked', entry)"
+        @deleted="deleteTravel(entry._id)"
+        @edit="$emit('edit', entry)"></TravelCard>
     </template>
-  </div>
+  </PaginationList>
 </template>
 
 <script lang="ts">
+import PaginationList from '../../elements/PaginationList.vue'
 import { defineComponent } from 'vue'
 import { TravelSimple } from '../../../../../common/types.js'
 import TravelCard from './TravelCard.vue'
+
 export default defineComponent({
   name: 'TravelCardList',
   emits: ['clicked', 'edit'],
-  data() {
-    return {
-      hasTravels: false,
-      hasMeta: false,
-      travels: [] as TravelSimple[],
-      meta: { countPages: -1, page: -1 }
-    }
-  },
-  components: { TravelCard },
+  components: { TravelCard, PaginationList },
   props: {
     endpoint: { type: String, required: true },
     showTraveler: { type: Boolean, default: false },
     showDropdown: { type: Boolean, default: false }
   },
   methods: {
-    async getTravels(page?: number): Promise<void> {
-      if (page === 0 || page) {
-        if (page < 1) {
-          page = 1
-        } else if (this.hasMeta && page > this.meta.countPages) {
-          page = this.meta.countPages
-        }
-      } else if (this.hasMeta) {
-        page = this.meta.page
-      }
-      var result = await this.$root.getter(this.endpoint, { page })
-      this.travels = result.data
-      this.hasTravels = true
-      this.meta = result.meta
-      this.hasMeta = true
-    },
     async deleteTravel(id: string): Promise<void> {
       const result = await this.$root.deleter('travel', { id: id })
       if (result) {
-        this.getTravels()
+        this.$refs.list.getTravels()
       }
     }
-  },
-  async beforeMount() {
-    await this.getTravels()
   }
 })
 </script>
