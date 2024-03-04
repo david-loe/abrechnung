bb0680ced4bbd6d1bec88df9adc49b63f1ae4738
david-loe <56305409+david-loe@users.noreply.github.com>
2024-03-04 10:36:04 +0000

    adding examine document file endpoints

--- backend/controller/documentFileController.ts
+++ backend/controller/documentFileController.ts
@@ -1,7 +1,7 @@
-import { Route, Get, Tags, Security, Request, Post, Body, Delete, Query, Produces, SuccessResponse } from 'tsoa'
-import { Controller, SetterBody } from './controller.js'
+import { Route, Get, Tags, Security, Request, Delete, Query, Produces, SuccessResponse } from 'tsoa'
+import { Controller } from './controller.js'
 import DocumentFile from '../models/documentFile.js'
-import { documentFileTypes, DocumentFile as IDocumentFile, _id } from '../../common/types.js'
+import { documentFileTypes, _id } from '../../common/types.js'
 import { Request as ExRequest } from 'express'
 import { mongo } from 'mongoose'
 
@@ -16,13 +16,13 @@ export class DocumentFileController extends Controller {
   @SuccessResponse(200)
   public async getDocumentFile(@Query() _id: _id, @Request() request: ExRequest) {
     const file = await DocumentFile.findOne({ _id: _id }).lean()
-  if (file && request.user!._id.equals(file.owner._id)) {
-    request.res?.setHeader('Content-Type', file.type)
-    request.res?.setHeader('Content-Length', (file.data as any as mongo.Binary).length().toString())
-    request.res?.send((file.data as any as mongo.Binary).buffer)
-  } else {
-    throw new Error('alerts.request.unauthorized')
-  }
+    if (file && request.user!._id.equals(file.owner._id)) {
+      request.res?.setHeader('Content-Type', file.type)
+      request.res?.setHeader('Content-Length', (file.data as any as mongo.Binary).length().toString())
+      request.res?.send((file.data as any as mongo.Binary).buffer)
+    } else {
+      throw new Error('alerts.request.unauthorized')
+    }
   }
 
   @Delete()
@@ -37,16 +37,30 @@ export class DocumentFileController extends Controller {
   }
 }
 
-@Tags('Admin', 'DocumentFile')
-@Route('api/admin/documentFile')
-@Security('cookieAuth', ['admin'])
+@Tags('Examine', 'DocumentFile')
+@Route('api/examine/documentFile')
+@Security('cookieAuth', ['user', 'examine/travel'])
+@Security('cookieAuth', ['user', 'examine/expenseReport'])
+@Security('cookieAuth', ['user', 'examine/healthCareCost'])
 export class DocumentFileAdminController extends Controller {
-  @Post()
-  public async postDocumentFile(@Body() requestBody: SetterBody<IDocumentFile>) {
-    return await this.setter(DocumentFile, { requestBody: requestBody })
+  @Get()
+  @Produces(documentFileTypes[0])
+  @Produces(documentFileTypes[1])
+  @Produces(documentFileTypes[2])
+  @SuccessResponse(200)
+  public async getDocumentFile(@Query() _id: _id, @Request() request: ExRequest) {
+    const file = await DocumentFile.findOne({ _id: _id }).lean()
+    if (file) {
+      request.res?.setHeader('Content-Type', file.type)
+      request.res?.setHeader('Content-Length', (file.data as any as mongo.Binary).length().toString())
+      request.res?.send((file.data as any as mongo.Binary).buffer)
+    } else {
+      throw new Error('No file found')
+    }
   }
+
   @Delete()
-  public async deleteDocumentFile(@Query() _id: _id) {
-    return await this.deleter(DocumentFile, { _id: _id })
+  public async deleteDocumentFile(@Query() _id: _id, @Request() request: ExRequest){
+    return await this.deleter(DocumentFile, { _id: _id})
   }
 }
