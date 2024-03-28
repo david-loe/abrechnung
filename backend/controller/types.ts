cf8aa459250a8cddb9f5cc2551327a47d55e658c
david-loe <56305409+david-loe@users.noreply.github.com>
2024-03-28 11:39:30 +0100

    fix weird tsoa type checking

--- backend/controller/types.ts
+++ backend/controller/types.ts
@@ -1,9 +1,9 @@
-import { Currency, CurrencyCode, DocumentFile, DocumentFileType, Money, Travel, TravelDay, TravelSimple, _id } from '../../common/types.js'
+import { Currency, CurrencyCode, DocumentFile, Money, Travel, TravelDay, TravelSimple, _id } from '../../common/types.js'
 import { SetterBody } from './controller.js'
 
 export type IdDocument<idType = _id> = idType | { _id: idType }
 
-export interface File<T extends DocumentFileType = DocumentFileType> extends Omit<DocumentFile<T>, 'data' | 'owner'> {
+export interface File extends Omit<DocumentFile, 'data' | 'owner'> {
   /**
    * @format binary
    */
