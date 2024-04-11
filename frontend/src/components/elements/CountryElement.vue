a59b88a1a3163861bf40821fdcb1bfdfa84eae3b
david-loe <56305409+david-loe@users.noreply.github.com>
2024-04-11 11:33:45 +0200

    country element

--- frontend/src/components/elements/CountryElement.vue
+++ frontend/src/components/elements/CountryElement.vue
@@ -6,11 +6,15 @@
       <!-- @vueform/multiselect copmonent -->
       <Multiselect
         v-bind="fieldOptions"
+        v-if="$root.countries.length > 0"
+        :options="$root.countries"
+        valueProp="_id"
+        searchable
+        :searchFilter="customFilterFunction"
         v-model="value"
         :classes="classes.select"
         :id="fieldId"
         :name="name"
-        :options="resolvedOptions"
         :disabled="isDisabled"
         :placeholder="Placeholder"
         :attrs="attrs"
@@ -27,7 +31,6 @@
         ref="input">
         <template
           v-for="(slotName, slotKey) in {
-            option: 'option',
             noresults: 'no-results',
             nooptions: 'no-options',
             afterlist: 'after-list',
@@ -45,10 +48,31 @@
           </slot>
         </template>
 
-        <template v-if="fieldOptions.mode == 'single'" #singlelabel="{ value }">
-          <slot name="single-label" :value="value" :el$="el$">
-            <component :is="fieldSlots['single-label']" :value="value" :el$="el$" />
-          </slot>
+        <template v-slot:singlelabel="{ value }">
+          <div class="position-absolute start-0 ms-3" :title="value.name[$i18n.locale]">
+            <span v-if="value.flag" class="me-1">{{ value.flag }}</span>
+            <span>{{ value._id }}</span>
+          </div>
+        </template>
+
+        <template v-slot:multiplelabel="{ values }">
+          <div class="position-absolute start-0 ms-3">
+            <span v-for="value of values" class="me-3" :title="value.name[$i18n.locale]">
+              <span v-if="value.flag" class="me-1">{{ value.flag }}</span>
+              <span>{{ value._id }}</span>
+            </span>
+          </div>
+        </template>
+
+        <template v-slot:option="{ option }">
+          <div class="row align-items-center" :title="option.name[$i18n.locale]">
+            <div v-if="option.flag" class="col-auto px-1">
+              <span class="fs-3">{{ option.flag }}</span>
+            </div>
+            <div class="col p-1 text-truncate">
+              <small>{{ option.name[$i18n.locale] }}</small>
+            </div>
+          </div>
         </template>
       </Multiselect>
     </template>
@@ -70,7 +94,29 @@ export default defineElement({
   ...SelectElement, // adding props, mixins, emits
   name: 'CountryElement',
   components: { Multiselect },
-  props: Object.assign(SelectElement.props, { native: { type: Boolean, default: false } }),
+  props: Object.assign(SelectElement.props, {
+    native: { type: Boolean, default: false }
+  }),
+  methods: {
+    customFilterFunction(option, search) {
+      console.log(option)
+      const name = option.name[this.$i18n.locale].toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) > -1
+      if (name) {
+        return name
+      }
+      if (option.alias && option.alias[this.$i18n.locale]) {
+        for (const alias of option.alias[this.$i18n.locale]) {
+          const result = alias.toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) > -1
+          if (result) {
+            return result
+          }
+        }
+      }
+
+      const code = option._id.toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) > -1
+      return code
+    }
+  },
   setup(props, context) {
     const element = SelectElement.setup(props, context)
     const defaultClasses = ref({
