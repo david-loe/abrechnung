223133e1a0ca7ad6e8ee4682a181e54ee8cf544f
david-loe <56305409+david-loe@users.noreply.github.com>
2024-04-12 13:18:39 +0200

    get countries with lump sums

--- frontend/src/components/settings/elements/CountryList.vue
+++ frontend/src/components/settings/elements/CountryList.vue
@@ -1,16 +1,16 @@
 <template>
-  <div v-if="$root.settings.accessIcons">
+  <div>
     <EasyDataTable
       class="mb-3"
       :rows-items="[5, 15, 25]"
       :rows-per-page="5"
       sort-by="name"
-      :items="$root.countries"
+      :items="countries"
       :filter-options="[
         {
           field: 'name',
           criteria: filter.name,
-          comparison: (value: Country['name'], criteria: string): boolean => value[$i18n.locale].toLowerCase().indexOf(criteria.toLowerCase()) !== -1
+          comparison: (value: Country['name'], criteria: string): boolean => value[$i18n.locale as Locale].toLowerCase().indexOf(criteria.toLowerCase()) !== -1
         }
       ]"
       :headers="[
@@ -69,8 +69,8 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import countryFormSchema from '../../../../../common/forms/country.json'
-import { Country, accesses } from '../../../../../common/types.js'
 import { getById } from '../../../../../common/scripts.js'
+import { Country, Locale, accesses } from '../../../../../common/types.js'
 
 interface Filter<T> {
   name: T
@@ -80,6 +80,7 @@ export default defineComponent({
   components: {},
   data() {
     return {
+      countries: [] as Country[],
       countryToEdit: undefined as Country | undefined,
       countryFormMode: 'add' as 'add' | 'edit',
       _showForm: false,
@@ -107,17 +108,25 @@ export default defineComponent({
       this.countryToEdit = country
       this._showForm = true
     },
+    async getCountries() {
+      const result = (await this.$root.getter<Country[]>('country', { additionalFields: ['lumpSums', 'lumpSumsFrom'] })).ok
+      if (result) {
+        this.countries = result.data
+      }
+    },
     async postCountry(country: Country) {
       console.log(country)
       const result = await this.$root.setter<Country>('admin/country', country)
       if (result.ok) {
         this._showForm = false
+        this.getCountries()
       }
       this.countryToEdit = undefined
     },
     async deleteCountry(country: Country) {
       const result = await this.$root.deleter('admin/country', { _id: country._id })
       if (result) {
+        this.getCountries()
       }
     },
     clickFilter(header: keyof Filter<string>) {
@@ -132,6 +141,7 @@ export default defineComponent({
   },
   async created() {
     await this.$root.load()
+    this.getCountries()
   }
 })
 </script>
