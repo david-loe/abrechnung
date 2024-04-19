661ebaf5b0c0ef8ebbe7b6189d19f12686aa5c84
david-loe <56305409+david-loe@users.noreply.github.com>
2024-04-19 16:35:22 +0200

    minor fixes

--- frontend/src/components/elements/ProjectSelector.vue
+++ frontend/src/components/elements/ProjectSelector.vue
@@ -14,13 +14,13 @@
 
     <v-select
       v-if="$root.user.settings.projects && $root.settings.userCanSeeAllProjects === $root.projects.length > 0"
-      :options="([] as any[]).concat($root.user.settings.projects, $root.projects)"
+      :options="getProjects()"
       :modelValue="modelValue"
       :placeholder="$t('labels.project')"
       @update:modelValue="(v: ProjectSimple) => $emit('update:modelValue', v)"
       :filter="filter"
       :disabled="disabled"
-      class="col-9">
+      :class="$root.organisations.length > 1 ? 'col-9' : ''">
       <template #option="{ identifier, name }: Project">
         <span>{{ identifier + (name ? ' ' + name : '') }}</span>
       </template>
@@ -51,6 +51,24 @@ export default defineComponent({
   },
   emits: ['update:modelValue'],
   methods: {
+    getProjects() {
+      const projects: ProjectSimple[] = [...this.$root.user.settings.projects]
+      if (this.$root.settings.userCanSeeAllProjects) {
+        for (const project of this.$root.projects) {
+          let alreadyIn = false
+          for (const userProject of this.$root.user.settings.projects) {
+            if (project._id === userProject._id) {
+              alreadyIn = true
+              break
+            }
+          }
+          if (!alreadyIn) {
+            projects.push(project)
+          }
+        }
+      }
+      return projects
+    },
     filter(options: (ProjectSimple | Project)[], search: string): ProjectSimple[] {
       return options.filter((option) => {
         if (this.$root.user.settings.organisation && option.organisation !== this.$root.user.settings.organisation._id) {
