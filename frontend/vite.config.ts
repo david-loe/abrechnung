dabf67c44c472b3eb4f79e1aacf1f7c88ba7f936
david-loe <56305409+david-loe@users.noreply.github.com>
2024-04-19 15:57:22 +0200

    edit dev frontend setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default {
-  plugins: [(vue as unknown as any)()],
+  plugins: [vue()],
   server: { port: 80, strictPort: true, host: '0.0.0.0' },
   preview: { port: 80, host: '0.0.0.0' },
   resolve: {
