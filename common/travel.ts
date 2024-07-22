63ad7e433ac33348c8f2df3a76e9a1e36c7421d4
david-loe <56305409+david-loe@users.noreply.github.com>
2024-07-22 19:35:24 +0000

    sorted lump sum calculation

--- common/travel.ts
+++ common/travel.ts
@@ -1,10 +1,10 @@
-import LumpSumCalculator from './lumpSumCalculator.js'
 import { datetimeToDate, getDayList, getDiffInDays } from './scripts.js'
 import {
   baseCurrency,
   Country,
   CountryCode,
   CountrySimple,
+  LumpSum,
   Meal,
   Place,
   PurposeSimple,
@@ -342,5 +342,43 @@ export class TravelValidator {
       }
     }
     return conflicts
+  }
+}
+
+export default class LumpSumCalculator {
+  fallBackLumpSumCountry: CountryCode
+  getCountryById: (id: CountryCode) => Promise<Country>
+
+  constructor(getCountryById: (id: CountryCode) => Promise<Country>, fallBackLumpSumCountry: CountryCode) {
+    this.getCountryById = getCountryById
+    this.fallBackLumpSumCountry = fallBackLumpSumCountry
+  }
+  async getLumpSum(country: Country, date: Date, special: string | undefined = undefined): Promise<LumpSum> {
+    if (country.lumpSumsFrom) {
+      const lumpSumFrom = await this.getCountryById(country.lumpSumsFrom)
+      return this.getLumpSum(lumpSumFrom, date)
+    } else if (country.lumpSums.length == 0) {
+      const fallBackLumpSumCountry = await this.getCountryById(this.fallBackLumpSumCountry)
+      return this.getLumpSum(fallBackLumpSumCountry, date)
+    } else {
+      var nearest = 0
+      for (var i = 0; i < country.lumpSums.length; i++) {
+        var diff = date.valueOf() - (country.lumpSums[i].validFrom as Date).valueOf()
+        if (diff >= 0 && diff < date.valueOf() - (country.lumpSums[nearest].validFrom as Date).valueOf()) {
+          nearest = i
+        }
+      }
+      if (date.valueOf() - (country.lumpSums[nearest].validFrom as Date).valueOf() < 0) {
+        throw new Error('No valid lumpSum found for Country: ' + country._id + ' for date: ' + date)
+      }
+      if (special && country.lumpSums[nearest].specials) {
+        for (const lumpSumSpecial of country.lumpSums[nearest].specials!) {
+          if (lumpSumSpecial.city === special) {
+            return lumpSumSpecial
+          }
+        }
+      }
+      return country.lumpSums[nearest]
+    }
   }
 }
