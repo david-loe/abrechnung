077d65417e083d01e55a0e4af29e2ce67aeb7ce3
Sarah <sarah.gemperlein@gmail.com>
2024-10-18 16:49:20 +0200

    putting data in indexeddb is working, stll commented Code for cache and data that is not working yet

--- frontend/sw.ts
+++ frontend/sw.ts
@@ -2,16 +2,15 @@
 import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
 import { NavigationRoute, registerRoute, setDefaultHandler } from 'workbox-routing'
 import { CacheFirst, NetworkFirst, NetworkOnly } from 'workbox-strategies'
-
 declare var self: ServiceWorkerGlobalScope
 export default {}
 precacheAndRoute(self.__WB_MANIFEST)
 let cacheName = 'abrechnung'
-
+const urlsToFetch = ['/backend/healthCareCost?limit=12', '/backend/expenseReport?limit=12', '/backend/travel?limit=12']
 self.addEventListener('install', (event) => {
   event.waitUntil(
     (async () => {
-      const cache = await caches.open(cacheName).then((cache) => cache.addAll(['user', 'backend/user']))
+      // const cache = await caches.open(cacheName).then((cache) => cache.addAll(['user', 'backend/user']))
       // Setting {cache: 'reload'} in the new request will ensure that the response
       // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
       // await cache.put(new Request('/backend/settings'), await fetch('/backend/settings'))
@@ -19,6 +18,14 @@ self.addEventListener('install', (event) => {
     })()
   )
 })
+
+self.addEventListener('activate', (event) => {
+  console.log('activate event triggerd')
+  for (const url of urlsToFetch) {
+    fetchAndStoreUrl(url)
+  }
+})
+
 setDefaultHandler(new NetworkOnly())
 // to allow work offline
 registerRoute(new NavigationRoute(createHandlerBoundToURL('index.html')))
@@ -36,7 +43,157 @@ registerRoute(
   })
 )
 
-self.addEventListener('fetch', (event) => {
-  console.log('sw weiterleitung genutzt')
-  event.respondWith(fetch(event.request))
-})
+function openDatabase(): Promise<IDBDatabase> {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open('myDatabase', 1)
+
+    request.onupgradeneeded = (event) => {
+      const db = (event.target as IDBOpenDBRequest).result
+      db.createObjectStore('urls', { keyPath: 'id' })
+    }
+
+    request.onsuccess = (event) => {
+      resolve((event.target as IDBOpenDBRequest).result)
+    }
+
+    request.onerror = (event) => {
+      reject((event.target as IDBOpenDBRequest).error)
+    }
+  })
+}
+
+// aktuell geht die Funktion davon aus, dass die URL auf jeden fall limit enthält und reportTypes abfragt, zu denen mehr gesucht werden
+async function fetchAndStoreUrl(url: string) {
+  const db = await openDatabase()
+  try {
+    const response = await fetch(url) // Daten von der URL abrufen
+    if (response.ok) {
+      const res = await response.json() // Antwortdaten verarbeiten
+      const transaction = db.transaction('urls', 'readwrite')
+      const store = transaction.objectStore('urls')
+      store.put({ id: url, res })
+      const shortenedUrl = url.replace(/\?limit=\d+/, '').replace('/backend/', '')
+      let reportType: 'travel' | 'expenseReport' | 'healthCareCost' | null = null
+      if (shortenedUrl == 'travel') {
+        reportType = 'travel'
+      } else if (shortenedUrl == 'expenseReport') {
+        reportType = 'expenseReport'
+      } else if (shortenedUrl == 'healthCareCost') {
+        reportType = 'healthCareCost'
+      }
+      console.log(res.data)
+      for (let i = 0; i < res.data.length; i++) {
+        console.log(res.data[i]._id + ' ' + reportType)
+        await fetchDetail(reportType, res.data[i]._id)
+      }
+      // Speichern in der IndexedDB
+      // Füge das Objekt in die IndexedDB ein
+
+      return new Promise<void>((resolve, reject) => {
+        transaction.oncomplete = () => {
+          resolve()
+        }
+        transaction.onerror = (event) => {
+          reject((event.target as IDBTransaction).error)
+        }
+      })
+    }
+  } catch (error) {
+    console.error(`Fehler beim Abrufen der URL ${url}:`, error)
+  }
+}
+
+async function fetchDetail(reportType: 'travel' | 'expenseReport' | 'healthCareCost' | null, id: string) {
+  if (reportType != null) {
+    const db = await openDatabase()
+    let url =
+      '/backend/' +
+      reportType +
+      '?id=' +
+      id +
+      (reportType == 'travel' ? '&additionalFields=stages&additionalFields=expenses&additionalFields=days' : '&additionalFields=expenses')
+    console.log(url)
+    try {
+      const response = await fetch(url) // Daten von der URL abrufen
+      if (response.ok) {
+        const res = await response.json() // Antwortdaten verarbeiten
+        const transaction = db.transaction('urls', 'readwrite')
+        const store = transaction.objectStore('urls')
+        store.put({ id: url, res })
+
+        return new Promise<void>((resolve, reject) => {
+          transaction.oncomplete = () => {
+            resolve()
+          }
+          transaction.onerror = (event) => {
+            reject((event.target as IDBTransaction).error)
+          }
+        })
+      }
+    } catch (error) {
+      console.error(`Fehler beim Abrufen der URL ${url}:`, error)
+    }
+  }
+}
+
+// // Route für travel, um die IDs bei der ersten Anfrage zu cachen
+// registerRoute(
+//   ({ request }) => /\/backend\/travel\?limit=/.test(request.url),
+//   async (params) => {
+//     const response = await fetch(params.request) // Initiale Travel-Anfrage
+//     const data = await response.json() // Antwort parsen
+
+//     // Sofortige Rückgabe der initialen Antwort
+//     const clonedResponse = response.clone()
+
+//     // IDs extrahieren und für jede ID eine Anfrage absetzen
+//     const travelRequests = data.travels.map(async (travel: { id: any }) => {
+//       try {
+//         const travelResponse = await fetch(`/backend/travel?id=${travel.id}&additionalFields=expenses&additionalFields=days`)
+
+//         if (!travelResponse.ok) {
+//           throw new Error(`Error fetching travel details for ID ${travel.id}: ${travelResponse.status}`)
+//         }
+
+//         // Klone den Response, da wir ihn nicht direkt cachen können
+//         const travelResponseClone = travelResponse.clone()
+
+//         // Jedes travelDetail im Cache speichern
+//         const travelCache = await caches.open('backend-cache')
+//         await travelCache.put(travelResponseClone.url, travelResponseClone)
+//       } catch (error) {
+//         console.error(error)
+//       }
+//     })
+
+//     // Warte auf alle zusätzlichen Anfragen
+//     await Promise.all(travelRequests)
+
+//     return clonedResponse // Die Hauptantwort zurückgeben
+//   }
+// )
+
+// self.addEventListener('fetch', (event) => {
+//   // Prüfen, ob die angeforderte URL die von dir gewünschte API ist
+//   if (event.request.url.includes('/backend/travel')) {
+//     console.log('travel getter abgefangen')
+//     event.respondWith(fetch(event.request))
+//   }
+// })
+
+// self.addEventListener('fetch', (event) => {
+//   console.log('sw weiterleitung genutzt')
+//   event.respondWith(fetch(event.request))
+// })
+// self.addEventListener('message', (event) => {
+//   if (event.data && event.data.type === 'cacheTravelDetails') {
+//     const travelId = event.data.travelId
+//     const response = event.data.response
+
+//     // Öffne den Cache und speichere die Antwort für die Travel-Details
+//     caches.open('travel-details-cache').then((cache) => {
+//       cache.put(`/backend/travel?_id=${travelId}&additionalFields=stages&additionalFields=expenses&additionalFields=days`, response)
+//       console.log(`Cached travel details for travel ID: ${travelId}`)
+//     })
+//   }
+// })
