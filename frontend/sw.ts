17edddfa386c7792902e695a6d0888aa7e713619
Sarah <sarah.gemperlein@gmail.com>
2024-10-24 11:19:03 +0200

    offline working without admin routes and post events - putting everything into cache

--- frontend/sw.ts
+++ frontend/sw.ts
@@ -1,31 +1,32 @@
 /// <reference lib="webworker" />
 import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
 import { NavigationRoute, registerRoute, setDefaultHandler } from 'workbox-routing'
-import { CacheFirst, NetworkFirst, NetworkOnly } from 'workbox-strategies'
+import { NetworkFirst, NetworkOnly, StaleWhileRevalidate } from 'workbox-strategies'
 declare var self: ServiceWorkerGlobalScope
 export default {}
 precacheAndRoute(self.__WB_MANIFEST)
 let cacheName = 'abrechnung'
-const urlsToFetch = ['/backend/healthCareCost?limit=12', '/backend/expenseReport?limit=12', '/backend/travel?limit=12']
+const reportTypeToFetch = ['healthCareCost', 'expenseReport', 'travel']
+
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    (async () => {
-      let urlsToCache = await fetchAndCacheUrls(urlsToFetch)
-      const cache = await caches.open(cacheName).then((cache) => {
-        cache.addAll(urlsToCache)
-        cache.addAll(urlsToFetch)
-      })
-      console.log('installevent triggerd')
-    })()
-  )
+  console.log('installevent triggerd')
   self.skipWaiting()
 })
 
 self.addEventListener('activate', (event) => {
   console.log('activate event triggerd')
-  for (const url of urlsToFetch) {
-    // fetchAndStoreUrl(url)
-  }
+  event.waitUntil(
+    (async () => {
+      let urlsToCache = await fetchAndCacheUrls(reportTypeToFetch)
+      const cache = await caches.open(cacheName).then((cache) => {
+        cache.addAll(urlsToCache)
+      })
+      new NetworkFirst({
+        cacheName: cacheName
+      })
+    })()
+  )
+  console.log('activated')
 })
 
 setDefaultHandler(new NetworkOnly())
@@ -34,7 +35,7 @@ registerRoute(new NavigationRoute(createHandlerBoundToURL('index.html')))
 
 registerRoute(
   ({ request }) => request.destination === 'font',
-  new CacheFirst({
+  new StaleWhileRevalidate({
     cacheName: 'font-cache'
   })
 )
@@ -46,39 +47,154 @@ registerRoute(
 )
 registerRoute(
   ({ request }) => request.url.includes('/manifest.json'),
-  new NetworkFirst({
+  new StaleWhileRevalidate({
     cacheName: 'manifest-cache'
   })
 )
 
-self.addEventListener('fetch', (event) => {
-  // Hier prüfen wir, ob die Anfrage zu einer der gecachten Routen gehört
-  event.respondWith(
-    caches.open(cacheName).then((cache) => {
-      return cache.keys().then((keys) => {
-        // console.log('Cached keys:', keys)
-        return cache.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            console.log('Cache response')
-            // Wenn die Antwort im Cache ist, gib sie sofort zurück
-            return cachedResponse
-          }
-          console.log('Network Response')
-          console.log(event.request)
-          // Wenn die Antwort nicht im Cache ist, hole sie vom Netzwerk
-          return fetch(event.request)
-        })
-        // .then((networkResponse) => {
-        //   // Die neue Antwort kann ebenfalls im Cache gespeichert werden (optional)
-        //   // return caches.open(cacheName).then((cache) => {
-        //   //   cache.put(event.request, networkResponse.clone()) // Klonen, da der Response-Stream nur einmal gelesen werden kann
-        //   return networkResponse // Netzwerkantwort zurückgeben
-        // })
-      })
-    })
-    //   })
-  )
-})
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     //das soll network first sein.
+//     fetch(event.request)
+//       .then((networkResponse) => {
+//         // Wenn die Netzwerkanfrage erfolgreich ist, wird die Antwort im Cache gespeichert
+//         return caches.open(cacheName).then((cache) => {
+//           // Speichere die Antwort im Cache für zukünftige Anfragen
+//           cache.put(event.request, networkResponse.clone())
+//           return networkResponse // Gib die Netzwerkantwort zurück
+//         })
+//       })
+//       .catch(() => {
+//         // Falls die Netzwerkanfrage fehlschlägt, versuche die Antwort aus dem Cache zu holen
+//         return caches.match(event.request).then((cachedResponse) => {
+//           if (cachedResponse) {
+//             console.log('cached response')
+//             return cachedResponse // Gib die gecachte Antwort zurück
+//           }
+//           // Optional: Fallback falls keine gecachte Antwort vorhanden ist
+//           return new Response('Offline und keine Daten im Cache verfügbar.', { status: 504 })
+//         })
+//       })
+//   )
+
+//   // Hier prüfen wir, ob die Anfrage zu einer der gecachten Routen gehört
+//   // event.respondWith(
+//   //   caches.open(cacheName).then((cache) => {
+//   //     return cache.keys().then((keys) => {
+//   //       // console.log('Cached keys:', keys)
+//   //       return cache.match(event.request).then((cachedResponse) => {
+//   //         if (cachedResponse) {
+//   //           console.log('Cache response')
+//   //           // Wenn die Antwort im Cache ist, gib sie sofort zurück
+//   //           return cachedResponse
+//   //         }
+//   //         console.log('Network Response')
+//   //         console.log(event.request)
+//   //         // Wenn die Antwort nicht im Cache ist, hole sie vom Netzwerk
+//   //         return fetch(event.request)
+//   //       })
+//   //       // .then((networkResponse) => {
+//   //       //   // Die neue Antwort kann ebenfalls im Cache gespeichert werden (optional)
+//   //       //   // return caches.open(cacheName).then((cache) => {
+//   //       //   //   cache.put(event.request, networkResponse.clone()) // Klonen, da der Response-Stream nur einmal gelesen werden kann
+//   //       //   return networkResponse // Netzwerkantwort zurückgeben
+//   //       // })
+//   //     })
+//   //   })
+//   //   //   })
+//   // )
+// })
+
+async function fetchAndCacheUrls(reportTypes: string[]) {
+  let detailUrls: string[] = []
+  for (let reportType of reportTypes) {
+    let url = '/backend/' + reportType + '?limit=12'
+    detailUrls.push(url)
+    detailUrls.push('/backend/' + reportType + '/examiner')
+    try {
+      const response = await fetch(url) // Daten von der URL abrufen
+      if (response.ok) {
+        const res = await response.json() // Antwortdaten verarbeiten
+        for (let i = 0; i < res.data.length; i++) {
+          detailUrls.push(
+            '/backend/' +
+              reportType +
+              '?_id=' +
+              res.data[i]._id +
+              (reportType == 'travel'
+                ? '&additionalFields=stages&additionalFields=expenses&additionalFields=days'
+                : '&additionalFields=expenses')
+          )
+        }
+      }
+    } catch (error) {
+      console.error(`Fehler beim Abrufen der URL ${url}:`, error)
+    }
+  }
+  return detailUrls
+}
+
+// // Route für travel, um die IDs bei der ersten Anfrage zu cachen
+// registerRoute(
+//   ({ request }) => /\/backend\/travel\?limit=/.test(request.url),
+//   async (params) => {
+//     const response = await fetch(params.request) // Initiale Travel-Anfrage
+//     const data = await response.json() // Antwort parsen
+
+//     // Sofortige Rückgabe der initialen Antwort
+//     const clonedResponse = response.clone()
+
+//     // IDs extrahieren und für jede ID eine Anfrage absetzen
+//     const travelRequests = data.travels.map(async (travel: { id: any }) => {
+//       try {
+//         const travelResponse = await fetch(`/backend/travel?id=${travel.id}&additionalFields=expenses&additionalFields=days`)
+
+//         if (!travelResponse.ok) {
+//           throw new Error(`Error fetching travel details for ID ${travel.id}: ${travelResponse.status}`)
+//         }
+
+//         // Klone den Response, da wir ihn nicht direkt cachen können
+//         const travelResponseClone = travelResponse.clone()
+
+//         // Jedes travelDetail im Cache speichern
+//         const travelCache = await caches.open('backend-cache')
+//         await travelCache.put(travelResponseClone.url, travelResponseClone)
+//       } catch (error) {
+//         console.error(error)
+//       }
+//     })
+
+//     // Warte auf alle zusätzlichen Anfragen
+//     await Promise.all(travelRequests)
+
+//     return clonedResponse // Die Hauptantwort zurückgeben
+//   }
+// )
+
+// self.addEventListener('fetch', (event) => {
+//   // Prüfen, ob die angeforderte URL die von dir gewünschte API ist
+//   if (event.request.url.includes('/backend/travel')) {
+//     console.log('travel getter abgefangen')
+//     event.respondWith(fetch(event.request))
+//   }
+// })
+
+// self.addEventListener('fetch', (event) => {
+//   console.log('sw weiterleitung genutzt')
+//   event.respondWith(fetch(event.request))
+// })
+// self.addEventListener('message', (event) => {
+//   if (event.data && event.data.type === 'cacheTravelDetails') {
+//     const travelId = event.data.travelId
+//     const response = event.data.response
+
+//     // Öffne den Cache und speichere die Antwort für die Travel-Details
+//     caches.open('travel-details-cache').then((cache) => {
+//       cache.put(`/backend/travel?_id=${travelId}&additionalFields=stages&additionalFields=expenses&additionalFields=days`, response)
+//       console.log(`Cached travel details for travel ID: ${travelId}`)
+//     })
+//   }
+// })
 
 function openDatabase(): Promise<IDBDatabase> {
   return new Promise((resolve, reject) => {
@@ -171,99 +287,3 @@ async function fetchDetail(reportType: 'travel' | 'expenseReport' | 'healthCareC
     }
   }
 }
-async function fetchAndCacheUrls(urls: string[]) {
-  let detailUrls: string[] = []
-  for (let url of urls) {
-    try {
-      const response = await fetch(url) // Daten von der URL abrufen
-      if (response.ok) {
-        const res = await response.json() // Antwortdaten verarbeiten
-        const shortenedUrl = url.replace(/\?limit=\d+/, '').replace('/backend/', '')
-        let reportType: 'travel' | 'expenseReport' | 'healthCareCost' | null = null
-        if (shortenedUrl == 'travel') {
-          reportType = 'travel'
-        } else if (shortenedUrl == 'expenseReport') {
-          reportType = 'expenseReport'
-        } else if (shortenedUrl == 'healthCareCost') {
-          reportType = 'healthCareCost'
-        }
-        for (let i = 0; i < res.data.length; i++) {
-          detailUrls.push(
-            '/backend/' +
-              reportType +
-              '?_id=' +
-              res.data[i]._id +
-              (reportType == 'travel'
-                ? '&additionalFields=stages&additionalFields=expenses&additionalFields=days'
-                : '&additionalFields=expenses')
-          )
-        }
-      }
-    } catch (error) {
-      console.error(`Fehler beim Abrufen der URL ${url}:`, error)
-    }
-  }
-  return detailUrls
-}
-
-// // Route für travel, um die IDs bei der ersten Anfrage zu cachen
-// registerRoute(
-//   ({ request }) => /\/backend\/travel\?limit=/.test(request.url),
-//   async (params) => {
-//     const response = await fetch(params.request) // Initiale Travel-Anfrage
-//     const data = await response.json() // Antwort parsen
-
-//     // Sofortige Rückgabe der initialen Antwort
-//     const clonedResponse = response.clone()
-
-//     // IDs extrahieren und für jede ID eine Anfrage absetzen
-//     const travelRequests = data.travels.map(async (travel: { id: any }) => {
-//       try {
-//         const travelResponse = await fetch(`/backend/travel?id=${travel.id}&additionalFields=expenses&additionalFields=days`)
-
-//         if (!travelResponse.ok) {
-//           throw new Error(`Error fetching travel details for ID ${travel.id}: ${travelResponse.status}`)
-//         }
-
-//         // Klone den Response, da wir ihn nicht direkt cachen können
-//         const travelResponseClone = travelResponse.clone()
-
-//         // Jedes travelDetail im Cache speichern
-//         const travelCache = await caches.open('backend-cache')
-//         await travelCache.put(travelResponseClone.url, travelResponseClone)
-//       } catch (error) {
-//         console.error(error)
-//       }
-//     })
-
-//     // Warte auf alle zusätzlichen Anfragen
-//     await Promise.all(travelRequests)
-
-//     return clonedResponse // Die Hauptantwort zurückgeben
-//   }
-// )
-
-// self.addEventListener('fetch', (event) => {
-//   // Prüfen, ob die angeforderte URL die von dir gewünschte API ist
-//   if (event.request.url.includes('/backend/travel')) {
-//     console.log('travel getter abgefangen')
-//     event.respondWith(fetch(event.request))
-//   }
-// })
-
-// self.addEventListener('fetch', (event) => {
-//   console.log('sw weiterleitung genutzt')
-//   event.respondWith(fetch(event.request))
-// })
-// self.addEventListener('message', (event) => {
-//   if (event.data && event.data.type === 'cacheTravelDetails') {
-//     const travelId = event.data.travelId
-//     const response = event.data.response
-
-//     // Öffne den Cache und speichere die Antwort für die Travel-Details
-//     caches.open('travel-details-cache').then((cache) => {
-//       cache.put(`/backend/travel?_id=${travelId}&additionalFields=stages&additionalFields=expenses&additionalFields=days`, response)
-//       console.log(`Cached travel details for travel ID: ${travelId}`)
-//     })
-//   }
-// })
