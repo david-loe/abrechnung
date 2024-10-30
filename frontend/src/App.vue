8f4041486711be451fa8cc662dee90e7a07fcf60
Sarah <sarah.gemperlein@gmail.com>
2024-10-30 11:35:32 +0100

    pushNotification subscription moved to load() - use it after user logged in

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -217,7 +217,37 @@ export default defineComponent({
             this.$vueform.i18n.locale = this.user.settings.language
             this.$formatter.setLocale(this.user.settings.language)
             this.auth = true
+            //das funktioniert zumindest in Chrome am Mac schonmal
+            if ('PushManager' in window && import.meta.env.VITE_PUBLIC_VAPID_KEY) {
+              let options = { userVisibleOnly: true, applicationServerKey: import.meta.env.VITE_PUBLIC_VAPID_KEY }
+              window.navigator.serviceWorker.getRegistration().then(async (registration) => {
+                if (registration) {
+                  registration.pushManager.subscribe(options).then(async (subscription) => {
+                    let existingPushsubscription = this.user.push.subscriptions.find((sub) => sub.endpoint === subscription.endpoint)
+                    if (!existingPushsubscription) {
+                      console.log('true')
+                      let result = await fetch('/backend/subscribe', {
+                        // aus komischen gründen wird die route nicht im chache gespeichert - ist praktisch, dürfte aber nicht so sein
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(subscription)
+                      })
+                      let data = await result.json()
+                      this.user.push.subscriptions.push(data.subscription)
+                      fetch('/backend/user/subscriptions', {
+                        // gleiches hier mit dem Cache
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(this.user.push)
+                      })
+                      console.log('subscription added')
+                    }
+                  })
+                }
+              })
+            }
           }
+          console.log(this.user)
           this.isOffline = !navigator.onLine // braucht man irgendwie für Safari & Firefox?
           this.loadState = 'LOADED'
         })
