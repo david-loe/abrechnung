4ea4520f1f044dd4f8da307ed3f871824cfd5c95
Sarah <sarah.gemperlein@gmail.com>
2024-10-30 11:35:53 +0100

    make public vapid key aviable in Frontend

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,4 +1,5 @@
 interface ImportMetaEnv {
+  readonly VITE_PUBLIC_VAPID_KEY: string
   readonly VITE_APP_TITLE: string
   readonly VITE_I18N_LOCALE: import('../../common/types').Locale
   readonly VITE_I18N_FALLBACK_LOCALE: import('../../common/types').Locale
