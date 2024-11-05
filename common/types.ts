b0abb801b4298cf54fc195b3c483448e6c2f9cb1
Sarah <sarah.gemperlein@gmail.com>
2024-11-05 16:50:50 +0100

    moved subscription to sessions

--- common/types.ts
+++ common/types.ts
@@ -176,9 +176,6 @@ export interface User extends UserSimple {
     organisation?: OrganisationSimple | null
     showInstallBanner: boolean
   }
-  push: {
-    subscriptions: Subscription[]
-  }
 
   vehicleRegistration?: DocumentFile[] | null
   token?: Token | null
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -219,7 +219,6 @@ export default defineComponent({
             this.$vueform.i18n.locale = this.user.settings.language
             this.$formatter.setLocale(this.user.settings.language)
             this.auth = true
-            //das funktioniert zumindest in Chrome am Mac schonmal
           }
           console.log(this.user)
           this.isOffline = !navigator.onLine // braucht man irgendwie für Safari & Firefox?
@@ -406,36 +405,15 @@ export default defineComponent({
               userVisibleOnly: true,
               applicationServerKey: this.urlBase64ToUint8Array(import.meta.env.VITE_PUBLIC_VAPID_KEY)
             }
-            console.log(options)
             window.navigator.serviceWorker.getRegistration().then(async (registration) => {
               if (registration) {
                 await registration.pushManager.subscribe(options).then(async (subscription) => {
-                  console.log(subscription)
-                  let existingPushsubscription = this.user.push.subscriptions.find((sub) => sub.endpoint === subscription.endpoint)
-                  console.log('already exits: ' + existingPushsubscription)
-                  if (existingPushsubscription) {
-                    console.log('true')
-                    let result = await fetch('/backend/subscribe', {
-                      // aus komischen gründen wird die route nicht im chache gespeichert - ist praktisch, dürfte aber nicht so sein
-                      method: 'POST',
-                      headers: { 'Content-Type': 'application/json' },
-                      body: JSON.stringify(subscription)
-                    })
-                    let data = await result.json()
-                    console.log(data)
-                    this.user.push.subscriptions.push(data.subscription)
-                    // try {
-                    //   await fetch('/backend/user/subscriptions', {
-                    //     // gleiches hier mit dem Cache
-                    //     method: 'POST',
-                    //     headers: { 'Content-Type': 'application/json' },
-                    //     body: JSON.stringify(this.user.push)
-                    //   })
-                    //   console.log('subscription added')
-                    // } catch (err) {
-                    //   console.log(err)
-                    // }
-                  }
+                  console.log('true')
+                  await fetch('/backend/subscribe', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(subscription)
+                  })
                 })
               }
             })
