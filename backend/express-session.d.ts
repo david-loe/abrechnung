d492dc729673f8d7ee6d06c089b42b07d1d92fc9
david-loe <56305409+david-loe@users.noreply.github.com>
2025-02-05 13:04:46 +0000

    optimize push notifications

--- backend/express-session.d.ts
+++ backend/express-session.d.ts
@@ -2,13 +2,12 @@ import 'express-session'
 import type { Types } from 'mongoose'
 import { PushSubscription } from 'web-push'
 
-// adding subscription and passport to SessionData interface for saving the Push Subscription in th session
 declare module 'express-session' {
   interface SessionData {
     subscription?: PushSubscription
-    passport?: {
-      user?: {
-        _id?: Types.ObjectId
+    passport: {
+      user: {
+        _id: Types.ObjectId
       }
     }
   }
--- backend/push.ts
+++ backend/push.ts
@@ -1,55 +1,43 @@
 import { SessionData } from 'express-session'
-import mongoose from 'mongoose'
 import webpush from 'web-push'
 import { User } from './../common/types.js'
 import { sessionStore } from './app.js'
 
 export async function sendPushNotification(title: string, body: string, users: User[], url: string) {
-  let payload = {
+  const payload = {
     title: title,
     body: body,
     url: url
   }
+  const allSessions = await getAllSessions()
+  for (const user of users) {
+    const userSessions = allSessions.filter((session) => session.subscription && user._id.equals(session.passport.user._id))
+    const latestSession = userSessions.reduce(
+      (max, session) => (new Date(session.cookie.expires!).valueOf() > new Date(max.cookie.expires!).valueOf() ? session : max),
+      userSessions[0]
+    )
 
-  for (let i = 0; i < users.length; i++) {
-    let sessions = await findSessionsByUserId(users[i]._id)
-    for (const session of sessions) {
-      if (session.subscription) {
-        webpush
-          .sendNotification(session.subscription, JSON.stringify(payload), {
-            vapidDetails: {
-              subject: process.env.VITE_FRONTEND_URL,
-              publicKey: process.env.VITE_PUBLIC_VAPID_KEY,
-              privateKey: process.env.VITE_PRIVATE_VAPID_KEY
-            }
-          })
-          .then((response) => console.log('Benachrichtigung gesendet:', response))
-          .catch((error) => console.error('Fehler beim Senden der Benachrichtigung:', error))
+    webpush.sendNotification(latestSession.subscription!, JSON.stringify(payload), {
+      vapidDetails: {
+        subject: process.env.VITE_FRONTEND_URL,
+        publicKey: process.env.VITE_PUBLIC_VAPID_KEY,
+        privateKey: process.env.VITE_PRIVATE_VAPID_KEY
       }
-    }
+    })
   }
 }
 
-async function findSessionsByUserId(userId: mongoose.Types.ObjectId) {
-  try {
-    const sessions: SessionData[] = await new Promise<SessionData[]>((resolve, reject) => {
-      sessionStore.all((err, sessions) => {
-        if (err) {
-          return reject(err)
-        }
-        if (Array.isArray(sessions)) {
-          resolve(sessions)
-        }
-      })
+function getAllSessions() {
+  return new Promise<SessionData[]>((resolve, reject) => {
+    sessionStore.all((err, sessions) => {
+      if (err) {
+        return reject(err)
+      }
+      if (Array.isArray(sessions)) {
+        resolve(sessions)
+      } else {
+        resolve([])
+      }
     })
-    const userSessions = sessions.filter((session) => {
-      return (
-        session.passport && session.passport.user && session.passport.user._id && session.passport.user._id.toString() === userId.toString()
-      )
-    })
-    return userSessions
-  } catch (error) {
-    console.error('Fehler beim Abrufen der Sessions:', error)
-    return []
-  }
+  })
 }
