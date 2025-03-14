5e9cb2e0dc958abbf7fdbfdfef3b33303670035d
david-loe <56305409+david-loe@users.noreply.github.com>
2025-03-14 16:31:50 +0100

    start with table

--- frontend/src/components/elements/PaginationList.vue
+++ frontend/src/components/elements/PaginationList.vue
@@ -1,172 +1,85 @@
 <template>
-  <div style="min-height: 120px">
-    <template v-if="hasData">
-      <template v-if="showSearch">
-        <div v-if="!search" class="row justify-content-end mb-2">
-          <div class="col-auto">
-            <button type="button" class="btn btn-light" @click="search = true"><i class="bi bi-search"></i></button>
-          </div>
-        </div>
-        <div v-else class="row mb-2">
-          <div class="col">
-            <div class="row justify-content-center align-items-center">
-              <div class="col-auto">
-                <form @submit.prevent="getData(meta.page)">
-                  <div class="row">
-                    <div class="col-auto">
-                      <div class="input-group mb-3">
-                        <input type="text" class="form-control" :placeholder="$t('labels.name')" v-model="searchFilter.name.$regex" />
-                        <button class="btn btn-outline-secondary" type="submit"><i class="bi bi-search"></i></button>
-                      </div>
-                    </div>
-                    <div class="col-auto">
-                      <UserSelector
-                        v-model="searchFilter.owner"
-                        @update:model-value="getData(meta.page)"
-                        :placeholder="$t('labels.owner')"></UserSelector>
-                    </div>
-                    <div class="col-auto">
-                      <ProjectSelector
-                        v-model="searchFilter.project"
-                        @update:model-value="getData(meta.page)"
-                        :org-select-split="5"
-                        style="min-width: 184px"></ProjectSelector>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-          <div class="col-auto ms-auto">
-            <button type="button" class="btn btn-light" @click="clearSearch()"><i class="bi bi-x-lg"></i></button>
-          </div>
-        </div>
-      </template>
-
-      <div class="row justify-content-center gx-4 gy-2">
-        <div class="col-auto" v-for="entry of data" :key="entry._id">
-          <slot name="entry" :entry="entry"></slot>
-        </div>
-      </div>
-      <div v-if="data.length === 0" class="alert alert-light" role="alert">
-        {{ $t('alerts.noData.' + endpoint) }}
-      </div>
-      <div v-else-if="meta && meta.countPages > 1" class="mx-auto text-secondary" style="width: fit-content">
-        <span class="me-2 p-2" style="cursor: pointer" @click="getData(meta.page - 1)">&lt;</span>
-        <span
-          v-for="x of meta.countPages"
-          :class="'me-2 p-2' + (meta.page == x ? ' fs-5' : '')"
-          :key="x"
-          :style="meta.page == x ? '' : 'cursor: pointer;'"
-          @click="meta.page == x ? null : getData(x)">
-          {{ x }}
-        </span>
-        <span class="p-2" style="cursor: pointer" @click="getData(meta.page + 1)">&gt;</span>
-      </div>
+  <EasyDataTable
+    class="mb-3"
+    :rows-items="[5, 15, 25]"
+    :rows-per-page="5"
+    v-model:server-options="serverOptions"
+    :server-items-length="serverItemsLength"
+    :loading="loading"
+    :items="items"
+    sort-by="name"
+    :headers="headers">
+    <template #item-state="{ state }">
+      <StateBadge :state="state"></StateBadge>
     </template>
-  </div>
+    <template #item-typ="report">
+      <template v-if="reportIsTravel(report)">
+        <i class="bi bi-airplane"></i>
+      </template>
+      <template v-else-if="reportIsHealthCareCost(report)">
+        <i class="bi bi-hospital"></i>
+      </template>
+      <template v-else>
+        <i class="bi bi-coin"></i>
+      </template>
+    </template>
+  </EasyDataTable>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import API from '@/api.js'
-import { defineComponent } from 'vue'
-import { Base64 } from '../../../../common/scripts.js'
-import ProjectSelector from './ProjectSelector.vue'
-import UserSelector from './UserSelector.vue'
+import i18n from '@/i18n.js'
+import { ref, watch } from 'vue'
+import type { Header, Item, ServerOptions } from 'vue3-easy-data-table'
+import { reportIsHealthCareCost, reportIsTravel } from '../../../../common/types'
+import StateBadge from './StateBadge.vue'
 
-export default defineComponent({
-  name: 'PaginationList',
-  components: { UserSelector, ProjectSelector },
-  data() {
-    return {
-      hasData: false,
-      hasMeta: false,
-      data: [] as any[],
-      meta: { countPages: -1, page: -1, limit: 12 },
-      search: false,
-      searchFilter: this.default()
-    }
-  },
-  props: {
-    endpoint: { type: String, required: true },
-    params: {
-      type: Object,
-      default: () => {
-        return { filter: {} }
-      }
-    },
-    showSearch: { type: Boolean, default: false }
-  },
-  methods: {
-    default() {
-      return {
-        name: { $regex: undefined as string | undefined, $options: 'i' },
-        owner: undefined as string | undefined,
-        project: undefined as string | undefined
-      }
-    },
-    async getData(page?: number): Promise<void> {
-      if (page === 0 || page) {
-        if (page < 1) {
-          page = 1
-        } else if (this.hasMeta && page > this.meta.countPages && this.meta.countPages > 0) {
-          page = this.meta.countPages
-        }
-      } else if (this.hasMeta) {
-        page = this.meta.page
-      }
-      const queryParams: any = structuredClone(this.params)
-      Object.assign(queryParams.filter, this.searchFilter)
-      for (const filterKey in queryParams.filter) {
-        if (queryParams.filter[filterKey] === null || queryParams.filter[filterKey] === undefined) {
-          delete queryParams.filter[filterKey]
-        } else if (!queryParams.filter[filterKey].$regex && queryParams.filter[filterKey].$options) {
-          delete queryParams.filter[filterKey]
-        }
-      }
+interface Props {
+  endpoint: string
+  showSearch: boolean
+}
 
-      const filterJSON = Base64.encode(JSON.stringify(queryParams.filter))
-      delete queryParams.filter
-      queryParams.filterJSON = filterJSON !== 'e30=' ? filterJSON : undefined
+const props = defineProps<Props>()
 
-      let result = (await API.getter<any[]>(this.endpoint, Object.assign({}, queryParams, { page, limit: this.meta.limit }))).ok
-      if (result) {
-        this.data = result.data
-        this.hasData = true
-        this.meta = result.meta
-        this.hasMeta = true
-      }
-      if (navigator.onLine) {
-        this.getRoutesForStore()
-      }
-    },
-    getRoutesForStore() {
-      //calling th detail routes of reports to trigger saving the data in the IndexedDB for offline usage
-      if (this.hasData) {
-        for (let i = 0; i < this.data.length; i++) {
-          fetch(
-            import.meta.env.VITE_BACKEND_URL +
-              '/' +
-              this.endpoint +
-              '?_id=' +
-              this.data[i]._id +
-              (this.endpoint == 'travel'
-                ? '&additionalFields=stages&additionalFields=expenses&additionalFields=days'
-                : '&additionalFields=expenses')
-          )
-        }
-      }
-    },
-    clearSearch() {
-      this.searchFilter = this.default()
-      this.search = false
-      this.getData(this.meta.page)
-    }
-  },
-  async created() {
-    await this.getData()
-  }
+const headers: Header[] = [
+  { text: '', value: 'typ' }, //@ts-ignore
+  { text: i18n.global.t('labels.name'), value: 'name' },
+  { text: i18n.global.t('labels.project'), value: 'project.identifier', sortable: true },
+  { text: i18n.global.t('labels.state'), value: 'state' }
+]
+
+const items = ref<Item[]>([])
+
+const loading = ref(false)
+const serverItemsLength = ref(0)
+const serverOptions = ref<ServerOptions>({
+  page: 1,
+  rowsPerPage: 5,
+  sortBy: 'age',
+  sortType: 'desc'
 })
+
+const loadFromServer = async () => {
+  loading.value = true
+  let result = (await API.getter<any[]>(props.endpoint, { page: serverOptions.value.page, limit: serverOptions.value.rowsPerPage })).ok
+  if (result) {
+    items.value = result.data
+    serverItemsLength.value = result.meta.count
+    console.log(result.meta)
+  }
+  loading.value = false
+}
+
+// initial load
+loadFromServer()
+
+watch(
+  serverOptions,
+  (value) => {
+    loadFromServer()
+  },
+  { deep: true }
+)
 </script>
 
 <style></style>
