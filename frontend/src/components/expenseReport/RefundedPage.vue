469a385984879e8ff5b25e53489d5e774e36a339
david-loe <56305409+david-loe@users.noreply.github.com>
2025-03-27 15:39:55 +0100

    feat: add view for refunded expense reports

---
+++ frontend/src/components/expenseReport/RefundedPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <h1 class="mb-3">{{ $t('accesses.refunded/expenseReport') }}</h1>
+    <ExpenseReportList class="mb-5" endpoint="refunded/expenseReport" :columns-to-hide="['state']"> </ExpenseReportList>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ExpenseReportList from './ExpenseReportList.vue'
+</script>
