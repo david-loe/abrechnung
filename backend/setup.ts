2d5bba113ef7d2dacd9b699290f70f3c1ab23057
david-loe <56305409+david-loe@users.noreply.github.com>
2025-04-24 17:13:16 +0200

    fix: sleep after app setup

--- backend/setup.ts
+++ backend/setup.ts
@@ -2,4 +2,8 @@ import APP from './app.js'
 import { disconnectDB } from './db.js'
 
 await APP()
+
+// sleep 5 seconds
+await new Promise((resolve) => setTimeout(resolve, 5000))
+
 await disconnectDB()
