a825e5335becae5031afdcadff3b0723b77c5aa5
david-loe <56305409+david-loe@users.noreply.github.com>
2025-07-08 16:40:25 +0200

    feat: update user language settings with browser language

--- frontend/src/appData.ts
+++ frontend/src/appData.ts
@@ -19,7 +19,7 @@ import {
 } from '../../common/types'
 import API from './api.js'
 import { formatter } from './formatter'
-import i18n from './i18n'
+import i18n, { getLanguageFromNavigator } from './i18n'
 import { logger } from './logger'
 import { vueform } from './main'
 
@@ -133,8 +133,11 @@ export class APP_DATA {
   }
   setUser(user: User) {
     this.user = user
+    const navLang = getLanguageFromNavigator()
     if (this.user.settings.hasUserSetLanguage) {
       setLanguage(this.user.settings.language)
+    } else if (this.user.settings.language !== navLang) {
+      API.setter('user/settings', { language: navLang } as User['settings'], {}, false)
     }
     //@ts-ignore
     logger.info(`${i18n.global.t('labels.user')}:`)
