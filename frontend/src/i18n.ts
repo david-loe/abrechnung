178c4a25e60c5b99ea10947acffb23d56534a3f7
david-loe <56305409+david-loe@users.noreply.github.com>
2025-07-08 15:34:46 +0200

    Get Frontend language from Users Browser Fixes #75

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,13 +1,21 @@
 import { createI18n, DefaultLocaleMessageSchema } from 'vue-i18n'
 import de from '../../common/locales/de.json' with { type: 'json' }
 import en from '../../common/locales/en.json' with { type: 'json' }
-import { defaultLocale, Locale } from '../../common/types.js'
+import { defaultLocale, Locale, locales } from '../../common/types.js'
 
 const defaultMessages = { de, en }
 
+function getLanguageFromNavigator() {
+  let language = navigator.language.split('-')[0] // language code without region
+  if (locales.indexOf(language as Locale) === -1) {
+    language = 'en' // default to English if the language is not supported
+  }
+  return language as Locale
+}
+
 export default createI18n<DefaultLocaleMessageSchema, Locale>({
   legacy: false,
-  locale: defaultLocale,
+  locale: getLanguageFromNavigator(),
   fallbackLocale: defaultLocale,
   messages: defaultMessages,
   globalInjection: true
