f250bbae70e89b0cc8d217b6cd8eec3e505a48f6
david-loe <56305409+david-loe@users.noreply.github.com>
2025-08-18 09:40:55 +0200

    fix: restore serving common folder via vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import { searchForWorkspaceRoot } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
@@ -21,7 +22,13 @@ export default {
       }
     })
   ],
-  server: { port: 80, strictPort: true, host: '0.0.0.0', allowedHosts: [process.env.VITE_FRONTEND_URL?.replace(/^https?:\/\//, '')] },
+  server: {
+    port: 80,
+    strictPort: true,
+    host: '0.0.0.0',
+    allowedHosts: [process.env.VITE_FRONTEND_URL?.replace(/^https?:\/\//, '')],
+    fs: { allow: [searchForWorkspaceRoot(process.cwd()), '../common'] }
+  },
   preview: { port: 80, host: '0.0.0.0' },
   resolve: { alias: { '@': resolve(__dirname, './src') } },
   build: { rollupOptions: { output: { entryFileNames: 'app-[hash].js' } } }
