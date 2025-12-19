a672172cc65a6765ded199e77c86efc0b3780fc0
david-loe <56305409+david-loe@users.noreply.github.com>
2025-12-19 10:07:32 +0100

    add migration

--- backend/migrations.ts
+++ backend/migrations.ts
@@ -1,11 +1,7 @@
 import countries from 'abrechnung-common/data/countries.json' with { type: 'json' }
 import currencies from 'abrechnung-common/data/currencies.json' with { type: 'json' }
-import { PrinterSettings } from 'abrechnung-common/types.js'
-import { detectImageType } from 'abrechnung-common/utils/file.js'
-import mongoose, { Types } from 'mongoose'
+import mongoose from 'mongoose'
 import semver from 'semver'
-import { fetchAndUpdateLumpSums } from './db.js'
-import { formatter, reportPrinter } from './factory.js'
 import { logger } from './logger.js'
 import Settings from './models/settings.js'
 
@@ -13,394 +9,11 @@ export async function checkForMigrations() {
   const settings = await Settings.findOne()
   if (settings?.migrateFrom) {
     const migrateFrom = settings.migrateFrom
-    const minVersion = '2.0.0'
+    const minVersion = '2.3.3'
     if (semver.lt(migrateFrom, minVersion)) {
       throw new Error(`Migration from v${migrateFrom} to v${settings.version} not supported. Migrate to v${minVersion} first.`)
     }
 
-    if (semver.lte(migrateFrom, '2.0.1')) {
-      logger.info('Apply migration from v2.0.1: add help button settings')
-
-      await mongoose.connection
-        .collection('displaysettings')
-        .updateOne({}, { $set: { helpButton: { enabled: true, examinersMail: true, examinersMsTeams: true, customOptions: [] } } })
-    }
-
-    if (semver.lte(migrateFrom, '2.0.4')) {
-      logger.info('Apply migration from v2.0.4: rewrite states')
-
-      await mongoose.connection
-        .collection('displaysettings')
-        .updateOne(
-          {},
-          {
-            $set: {
-              stateColors: {
-                '-10': { color: '#E8998D', text: 'black' },
-                '0': { color: '#cae5ff', text: 'black' },
-                '10': { color: '#89BBFE', text: 'black' },
-                '20': { color: '#6f8ab7', text: 'white' },
-                '30': { color: '#615d6c', text: 'white' },
-                '40': { color: '#5E8C61', text: 'white' }
-              }
-            }
-          }
-        )
-      // biome-ignore-start lint/suspicious/noThenProperty: mongodb uses then
-      const switchTravelStates = (field: string) => ({
-        $switch: {
-          branches: [
-            { case: { $eq: [field, 'rejected'] }, then: -10 },
-            { case: { $eq: [field, 'appliedFor'] }, then: 0 },
-            { case: { $eq: [field, 'approved'] }, then: 10 },
-            { case: { $eq: [field, 'underExamination'] }, then: 20 },
-            { case: { $eq: [field, 'refunded'] }, then: 30 }
-          ],
-          default: field
-        }
-      })
-      await mongoose.connection
-        .collection('travels')
-        .updateMany({}, [
-          {
-            $set: {
-              state: switchTravelStates('$state'),
-              comments: {
-                $map: {
-                  input: '$comments',
-                  as: 'comment',
-                  in: { $mergeObjects: ['$$comment', { toState: switchTravelStates('$$comment.toState') }] }
-                }
-              }
-            }
-          }
-        ])
-      const switchExpenseReportStates = (field: string) => ({
-        $switch: {
-          branches: [
-            { case: { $eq: [field, 'inWork'] }, then: 10 },
-            { case: { $eq: [field, 'underExamination'] }, then: 20 },
-            { case: { $eq: [field, 'refunded'] }, then: 30 }
-          ],
-          default: field
-        }
-      })
-      await mongoose.connection
-        .collection('expensereports')
-        .updateMany({}, [
-          {
-            $set: {
-              state: switchExpenseReportStates('$state'),
-              comments: {
-                $map: {
-                  input: '$comments',
-                  as: 'comment',
-                  in: { $mergeObjects: ['$$comment', { toState: switchExpenseReportStates('$$comment.toState') }] }
-                }
-              }
-            }
-          }
-        ])
-      const switchHealthCareCostStates = (field: string) => ({
-        $switch: {
-          branches: [
-            { case: { $eq: [field, 'inWork'] }, then: 10 },
-            { case: { $eq: [field, 'underExamination'] }, then: 20 },
-            { case: { $eq: [field, 'underExaminationByInsurance'] }, then: 30 },
-            { case: { $eq: [field, 'refunded'] }, then: 30 }
-          ],
-          default: field
-        }
-      })
-      await mongoose.connection
-        .collection('healthcarecosts')
-        .updateMany({}, [
-          {
-            $set: {
-              state: switchHealthCareCostStates('$state'),
-              comments: {
-                $map: {
-                  input: '$comments',
-                  as: 'comment',
-                  in: { $mergeObjects: ['$$comment', { toState: switchHealthCareCostStates('$$comment.toState') }] }
-                }
-              }
-            }
-          }
-        ])
-      const switchAdvancesStates = (field: string) => ({
-        $switch: {
-          branches: [
-            { case: { $eq: [field, 'rejected'] }, then: -10 },
-            { case: { $eq: [field, 'appliedFor'] }, then: 0 },
-            { case: { $eq: [field, 'approved'] }, then: 30 },
-            { case: { $eq: [field, 'completed'] }, then: 30 }
-          ],
-          default: field
-        }
-      })
-      await mongoose.connection
-        .collection('advances')
-        .updateMany({}, [
-          {
-            $set: {
-              state: switchAdvancesStates('$state'),
-              comments: {
-                $map: {
-                  input: '$comments',
-                  as: 'comment',
-                  in: { $mergeObjects: ['$$comment', { toState: switchAdvancesStates('$$comment.toState') }] }
-                }
-              },
-              settledOn: '$log.approved.date'
-            }
-          }
-        ])
-      // biome-ignore-end lint/suspicious/noThenProperty: mongodb uses then
-      const logRenameTraAdv = (approvedState = 10) => ({
-        $rename: {
-          'log.rejected': 'log.-10',
-          'log.appliedFor': 'log.0',
-          'log.approved': `log.${approvedState}`,
-          'log.underExamination': 'log.20'
-        }
-      })
-      const logRenameExpHea = {
-        $rename: {
-          'log.inWork': 'log.10',
-          'log.underExamination': 'log.20'
-          // 'log.underExaminationByInsurance' left as is
-        }
-      }
-      await mongoose.connection.collection('travels').updateMany({}, logRenameTraAdv(10))
-      await mongoose.connection.collection('expensereports').updateMany({}, logRenameExpHea)
-      await mongoose.connection.collection('healthcarecosts').updateMany({}, logRenameExpHea)
-      await mongoose.connection.collection('advances').updateMany({}, logRenameTraAdv(30))
-      await mongoose.connection
-        .collection('users')
-        .updateMany(
-          {},
-          {
-            $rename: {
-              'access.approved/advance': 'access.book/advance',
-              'access.refunded/travel': 'access.book/travel',
-              'access.refunded/expenseReport': 'access.book/expenseReport',
-              'access.refunded/healthCareCost': 'access.book/healthCareCost'
-            }
-          }
-        )
-      await mongoose.connection
-        .collection('displaysettings')
-        .updateMany(
-          {},
-          {
-            $rename: {
-              'accessIcons.approved/advance': 'accessIcons.book/advance',
-              'accessIcons.refunded/travel': 'accessIcons.book/travel',
-              'accessIcons.refunded/expenseReport': 'accessIcons.book/expenseReport',
-              'accessIcons.refunded/healthCareCost': 'accessIcons.book/healthCareCost'
-            }
-          }
-        )
-    }
-    if (semver.lte(migrateFrom, '2.1.0')) {
-      logger.info('Apply migration from v2.1.0: add name display format setting')
-
-      await mongoose.connection.collection('displaysettings').updateOne({}, { $set: { nameDisplayFormat: 'givenNameFirst' } })
-    }
-    if (semver.lte(migrateFrom, '2.1.2')) {
-      logger.info('Apply migration from v2.1.2: rewrite log')
-
-      const advanceMap = { 0: 30, 30: 40, 40: 40 } as const
-      const otherMap = { 0: 10, 10: 20, 20: 30, 30: 40, 40: 40 } as const
-
-      function rewriteLog(
-        oldLog: {
-          [key in number]?: { date: Date; editor: mongoose.Types.ObjectId }
-        },
-        map: { [key in number]: number }
-      ) {
-        const newLog: { [key in number]?: { on: Date; by: mongoose.Types.ObjectId } } = {}
-        for (const key in oldLog) {
-          if (oldLog[key] && map[key]) {
-            newLog[map[key]] = { on: oldLog[key].date, by: oldLog[key].editor }
-          }
-        }
-        return newLog
-      }
-      async function bulkRewriteLog(collectionName: string) {
-        const docs = mongoose.connection.collection(collectionName).find({})
-        const map = collectionName === 'advances' ? advanceMap : otherMap
-        for await (const doc of docs) {
-          await mongoose.connection.collection(collectionName).updateOne({ _id: doc._id }, { $set: { log: rewriteLog(doc.log, map) } })
-        }
-      }
-      await bulkRewriteLog('travels')
-      await bulkRewriteLog('expensereports')
-      await bulkRewriteLog('healthcarecosts')
-      await bulkRewriteLog('advances')
-    }
-
-    if (semver.lte(migrateFrom, '2.1.3')) {
-      logger.info('Apply migration from v2.1.3: add approved travels')
-
-      await mongoose.connection.collection('travelsettings').updateOne({}, { $set: { defaultLastPlaceOfWork: 'destinationPlace' } })
-
-      const travels = await mongoose.connection
-        .model('Travel')
-        .find({ historic: false, state: { $gte: 10 } })
-        .lean() //APPROVED or higher
-      const approvedTravels = []
-      for (const travel of travels) {
-        try {
-          approvedTravels.push({
-            startDate: travel.startDate,
-            endDate: travel.endDate,
-            reason: travel.reason,
-            destinationPlace: travel.destinationPlace,
-            claimSpouseRefund: travel.claimSpouseRefund,
-            fellowTravelersNames: travel.fellowTravelersNames,
-            traveler: formatter.name(travel.owner.name),
-            approvedBy: formatter.name(travel.log[10]?.by.name), //APPROVED
-            approvedOn: travel.log[10]?.on as Date, //APPROVED
-            appliedForOn: travel.log[0]?.on || travel.createdAt, //APPLIED_FOR
-            reportId: travel._id,
-            organisationId: travel.project.organisation
-          })
-        } catch (e) {
-          logger.error(`Unable to convert travel (${travel._id}) to approvedTravel: ${e}`)
-        }
-      }
-      if (approvedTravels.length > 0) {
-        await mongoose.connection.collection('approvedtravels').insertMany(approvedTravels)
-      }
-    }
-    if (semver.lte(migrateFrom, '2.1.4')) {
-      logger.info('Apply migration from v2.1.4: fix wrong file types in document files')
-      const documentFilesCursor = await mongoose.connection
-        .model('DocumentFile')
-        .find({ type: { $in: ['image/jpeg', 'image/png'] } })
-        .lean()
-        .batchSize(50)
-        .cursor()
-      const bulkOps = []
-      for await (const file of documentFilesCursor) {
-        const detectedType = detectImageType(file.data.buffer)
-        if (detectedType && detectedType !== file.type) {
-          bulkOps.push({ updateOne: { filter: { _id: file._id }, update: { $set: { type: detectedType } } } })
-        }
-        if (bulkOps.length >= 100) {
-          await mongoose.connection.model('DocumentFile').bulkWrite(bulkOps)
-          bulkOps.length = 0
-        }
-      }
-      if (bulkOps.length) {
-        await mongoose.connection.model('DocumentFile').bulkWrite(bulkOps)
-      }
-    }
-    if (semver.lte(migrateFrom, '2.1.7')) {
-      logger.info('Apply migration from v2.1.7: fix typo fallBack to fallback')
-      await mongoose.connection
-        .collection('travelsettings')
-        .updateMany({}, { $rename: { fallBackLumpSumCountry: 'fallbackLumpSumCountry' } })
-    }
-    if (semver.lte(migrateFrom, '2.1.13')) {
-      logger.info('Apply migration from v2.1.13: set fontName')
-      await mongoose.connection.collection('printersettings').updateOne({}, { $set: { fontName: 'Inter' } })
-    }
-    if (semver.lte(migrateFrom, '2.1.14')) {
-      logger.info('Apply migration from v2.1.14: add reference numbers to existing documents')
-
-      async function addReference(collectionName: string) {
-        const collection = mongoose.connection.collection<{ reference: number; historic?: boolean }>(collectionName)
-        const cursor = collection.find({ historic: { $ne: true } }).sort({ _id: 1 })
-        let counter = 1
-        const batchSize = 1_000
-        let batch = []
-        for await (const doc of cursor) {
-          batch.push({ updateOne: { filter: { _id: doc._id }, update: { $set: { reference: counter++ } } } })
-          if (batch.length === batchSize) {
-            await collection.bulkWrite(batch)
-            batch = []
-          }
-        }
-        if (batch.length > 0) {
-          await collection.bulkWrite(batch)
-        }
-      }
-      await addReference('travels')
-      await addReference('expensereports')
-      await addReference('healthcarecosts')
-      await addReference('advances')
-    }
-    if (semver.lte(migrateFrom, '2.3.0')) {
-      logger.info('Apply migration from v2.3.0: set printer settings options')
-      await mongoose.connection
-        .collection('printersettings')
-        .updateOne(
-          {},
-          {
-            $set: {
-              options: {
-                travel: { reviewDates: true, metaInformation: true, project: true, comments: true, notes: true },
-                expenseReport: { reviewDates: true, metaInformation: true, project: true, comments: true, notes: true },
-                healthCareCost: { reviewDates: true, metaInformation: true, project: true, comments: true, notes: true },
-                advance: { reviewDates: true, metaInformation: true, project: true, comments: true, notes: true }
-              }
-            }
-          }
-        )
-      const printerSettings = await mongoose.connection.collection<PrinterSettings>('printersettings').findOne({})
-      if (printerSettings) {
-        reportPrinter.setSettings(printerSettings)
-      }
-    }
-    if (semver.lte(migrateFrom, '2.3.1')) {
-      logger.info('Apply migration from v2.3.1: fill report usage collection')
-      const reportUsageCol = mongoose.connection.collection('reportusages')
-      async function addReportUsage(collectionName: string, modelName: string) {
-        const col = mongoose.connection.collection<{
-          updatedAt: Date
-          reference: number
-          project: Types.ObjectId
-          historic?: boolean
-          state: number
-          log: { [key: number]: { on: Date; by: Types.ObjectId } }
-        }>(collectionName)
-        const cursor = col.find({ historic: { $ne: true }, state: { $gte: 30 } }).sort({ _id: 1 })
-        const batchSize = 500
-        let batch = []
-        for await (const doc of cursor) {
-          const organisationId = (
-            await mongoose.connection.collection<{ organisation: Types.ObjectId }>('projects').findOne({ _id: doc.project })
-          )?.organisation
-          batch.push({
-            reportId: doc._id,
-            reference: doc.reference,
-            reportModelName: modelName,
-            organisationId: organisationId,
-            projectId: doc.project,
-            createdAt: doc.log[30]?.on || doc.updatedAt
-          })
-          if (batch.length === batchSize) {
-            await reportUsageCol.insertMany(batch)
-            batch = []
-          }
-        }
-        if (batch.length > 0) {
-          await reportUsageCol.insertMany(batch)
-        }
-      }
-      await addReportUsage('travels', 'Travel')
-      await addReportUsage('expensereports', 'ExpenseReport')
-      await addReportUsage('healthcarecosts', 'HealthCareCost')
-      await addReportUsage('advances', 'Advance')
-    }
-    if (semver.lte(migrateFrom, '2.3.2')) {
-      logger.info('Apply migration from v2.3.2: refill lumpSums with validUntil')
-      await mongoose.connection.collection('countries').updateMany({}, { $set: { lumpSums: [] } })
-      await fetchAndUpdateLumpSums()
-    }
     if (semver.lte(migrateFrom, '2.3.3')) {
       logger.info('Apply migration from v2.3.3: update country and currency names')
 
@@ -417,6 +30,33 @@ export async function checkForMigrations() {
         currencyBatch.push({ updateOne: { filter: { _id: currency.code }, update: { $set: { name: currency.name } } } })
       }
       await currencyCol.bulkWrite(currencyBatch)
+
+      logger.info('Apply migration from v2.3.3: update advance offsetAgainst subject')
+      const advanceCol = mongoose.connection.collection<{ offsetAgainst: Record<string, unknown>[] }>('advances')
+      const advanceBatch = []
+      const cursor = advanceCol.find()
+      for await (const doc of cursor) {
+        const newOffsetAgainst = []
+        for (const offset of doc.offsetAgainst) {
+          let subject = ''
+          if (offset.report) {
+            const report = await mongoose
+              .model<{ name: string }>(offset.type as string)
+              .findOne({ _id: offset.report })
+              .lean()
+            console.log(report)
+            subject = report?.name || ''
+          }
+          newOffsetAgainst.push({
+            reportId: offset.report,
+            type: offset.report ? offset.type : 'offsetEntry',
+            subject,
+            amount: offset.amount
+          })
+        }
+        advanceBatch.push({ updateOne: { filter: { _id: doc._id }, update: { $set: { offsetAgainst: newOffsetAgainst } } } })
+      }
+      await advanceCol.bulkWrite(advanceBatch)
     }
 
     settings.migrateFrom = undefined
